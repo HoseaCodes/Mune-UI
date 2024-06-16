b879fa1a4bb777fce1e5ef28307c4007b2012e20
Dominique Hosea <dominique11h@yahoo.com>
2024-06-15 23:45:13 -0500

    Updated Footer to approved Web Design.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,61 +5,101 @@ import {
   FooterTitle,
   FooterLink,
   SubscribeLink,
-  SubscribeIcons,
-  NavLinks,
   AppComingSoon,
   PhoneIcon,
-  FooterInput,
-  FooterFormContainer,
-  FooterForm,
-  FooterFormWrapper,
-  FooterSubmitButton,
+  Form,
+  Input,
+  SubmitButton,
+  StoreLinks,
+  StoreButton,
+  SubscribeLinks,
+  FooterContent,
+  FooterTopText,
+  LegalSection,
+  NavigationSection,
+  SubscribeSection,
+  FooterSectionContainer,
+  FooterSection,
 } from '../styles/FooterStyles';
-import logo from '../assets/logo.png';
+import logo from '../assets/logo1.png';
 import phoneIcon from '../assets/phone-icon.png';
-import googlePlay from '../assets/google-play.png'; // Add Google Play image
-import appStore from '../assets/app-store.png'; // Add App Store image
+import googlePlayLogo from '../assets/google-play-logo.png'; // Add Google Play image
+import appStoreLogo from '../assets/app-store-logo.png'; // Add App Store image
 import {
   FaFacebook,
   FaInstagram,
   FaTiktok,
 } from 'react-icons/fa';
 
-const Footer = () => (
-  <FooterWrapper>
-    <FooterLogo src={logo} alt="Logo" />
-    <FooterTitle>Don't Sleep! Sign Up Now.</FooterTitle>
-    <FooterFormContainer>
-      <FooterFormWrapper>
-        <PhoneIcon src={phoneIcon} alt="Phone" />
-        <FooterForm>
-          <FooterInput
+const Footer: React.FC = () => {
+  return (
+    <FooterWrapper>
+      <FooterContent>
+        <FooterTopText>
+          Don't Sleep! Sign Up Now.
+        </FooterTopText>
+        <Form>
+          <Input
             type="text"
             placeholder="Enter your mobile number"
           />
-          <FooterSubmitButton>CTA</FooterSubmitButton>
-        </FooterForm>
-      </FooterFormWrapper>
-    </FooterFormContainer>
-    <AppComingSoon>App Coming Soon!</AppComingSoon>
-    <NavLinks>
-      <FooterLink href="/">Home</FooterLink>
-      <FooterLink href="/about">About</FooterLink>
-      <FooterLink href="/contact">Contact</FooterLink>
-      <FooterLink href="/subscribe">Subscribe</FooterLink>
-    </NavLinks>
-    <SubscribeIcons>
-      <SubscribeLink href="https://facebook.com">
-        <FaFacebook />
-      </SubscribeLink>
-      <SubscribeLink href="https://instagram.com">
-        <FaInstagram />
-      </SubscribeLink>
-      <SubscribeLink href="https://tiktok.com">
-        <FaTiktok />
-      </SubscribeLink>
-    </SubscribeIcons>
-  </FooterWrapper>
-);
+          <SubmitButton type="submit">
+            Get Mun-e
+          </SubmitButton>
+        </Form>
+      </FooterContent>
+      <FooterSectionContainer>
+        <FooterSection>
+          <FooterLogo src={logo} alt="Logo" />
+          <AppComingSoon>App Coming Soon!</AppComingSoon>
+          <StoreLinks>
+            <StoreButton href="https://play.google.com/store">
+              <img src={googlePlayLogo} alt="Google Play" />
+              <div>
+                <div>GET IT ON</div>
+                <div>Google Play</div>
+              </div>
+            </StoreButton>
+            <StoreButton href="https://www.apple.com/app-store">
+              <img src={appStoreLogo} alt="App Store" />
+              <div>
+                <div>Download on the</div>
+                <div>App Store</div>
+              </div>
+            </StoreButton>
+          </StoreLinks>
+        </FooterSection>
+        <LegalSection>
+          <FooterTitle>Legal</FooterTitle>
+          <FooterLink href="/terms">Terms of use</FooterLink>
+          <FooterLink href="/privacy">Privacy Policy</FooterLink>
+          <FooterLink href="/security">Security Policy</FooterLink>
+          <FooterLink href="/cookie-settings">Cookie Settings</FooterLink>
+        </LegalSection>
+        <NavigationSection>
+          <FooterTitle>Navigation</FooterTitle>
+          <FooterLink href="/">Home</FooterLink>
+          <FooterLink href="/about">About</FooterLink>
+          <FooterLink href="/contact">Contact</FooterLink>
+          <FooterLink href="/subscribe">Subscribe</FooterLink>
+        </NavigationSection>
+        <SubscribeSection>
+          <FooterTitle>Subscribe</FooterTitle>
+          <SubscribeLinks>
+            <SubscribeLink href="https://facebook.com">
+              <FaFacebook />
+            </SubscribeLink>
+            <SubscribeLink href="https://instagram.com">
+              <FaInstagram />
+            </SubscribeLink>
+            <SubscribeLink href="https://tiktok.com">
+              <FaTiktok />
+            </SubscribeLink>
+          </SubscribeLinks>
+        </SubscribeSection>
+      </FooterSectionContainer>
+    </FooterWrapper>
+  );
+};
 
 export default Footer;
--- src/styles/FooterStyles.ts
+++ src/styles/FooterStyles.ts
@@ -7,78 +7,25 @@ export const FooterWrapper = styled.footer`
   padding: ${spacing.large};
   display: flex;
   flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
   border-top-left-radius: 20px;
   border-top-right-radius: 20px;
   position: relative;
-  min-height: 300px;
+  min-height: 500px;
+`;
+
+export const FooterContent = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: ${spacing.large};
 `;
 
 export const FooterLogo = styled.img`
-  width: 100px;
-  margin-bottom: ${spacing.medium};
-`;
-
-export const FooterTitle = styled.h4`
-  font-size: 2rem;
-  font-weight: ${typography.fontWeight.bold};
-  text-align: left;
-  margin-bottom: ${spacing.medium};
-`;
-
-export const FooterFormContainer = styled.div`
-  background-color: white;
-  border-radius: 80px;
-  padding: 20px;
-  margin-bottom: ${spacing.large};
-  width: 100%;
-  max-width: 559px;
-  max-height: 92px;
-  display: flex;
-  justify-content: center; /* Center the content horizontally */
-  align-items: center; /* Center the content vertically */
-  gap: 10px;
-`;
-
-export const FooterFormWrapper = styled.div`
-  padding: 7px;
-  box-shadow: 0 0 0 2px black;
-  display: flex;
-  align-items: center;
-  width: 100%;
-  max-width: 479px;
-  max-height: 37px;
-  border-radius: 24px;
-`;
-
-export const FooterForm = styled.form`
-  display: flex;
-  align-items: center;
-  width: 100%;
-  border-radius: 30px;
-`;
-
-export const FooterInput = styled.input`
-  padding: ${spacing.small};
-  border: none;
-  outline: none;
-  font-size: ${typography.fontSize};
-  flex: 1;
-  border-radius: 30px 0 0 30px;
-  margin: 0 ${spacing.small};
-  &::placeholder {
-    color: black;
-  }
-`;
-
-export const FooterSubmitButton = styled.button`
-  padding: ${spacing.small} ${spacing.large};
-  background-color: black;
-  color: white;
-  border: none;
-  font-weight: ${typography.fontWeight.bold};
-  cursor: pointer;
-  border-radius: 30px;
+  width: 191px;
+  height: 90px;
+  margin-bottom: 50px;
 `;
 
 export const PhoneIcon = styled.img`
@@ -86,45 +33,180 @@ export const PhoneIcon = styled.img`
   margin-right: ${spacing.small};
 `;
 
-export const AppComingSoon = styled.p`
-  font-size: 1.2rem;
+export const FooterTopText = styled.h3`
+  font-size: 3rem;
   font-weight: ${typography.fontWeight.bold};
-  text-align: left;
+  text-align: center;
   margin-bottom: ${spacing.medium};
 `;
 
-export const NavLinks = styled.div`
+export const AppComingSoon = styled.p`
+  font-size: 1.5rem;
+  font-weight: ${typography.fontWeight.bold};
+  text-align: left;
+  margin-top: -2rem;
+  margin-bottom: 1.5rem;
+`;
+
+export const FooterSectionContainer = styled.div`
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  margin-bottom: ${spacing.large};
+  justify-content: space-between; /* Changed to space-between to reduce space */
+  width: 100%;
+  padding-right: ${spacing.large};
+  gap: ${spacing.small};
+`;
+
+export const FooterSection = styled.div`
+  flex: 1;
+  margin: 0 0 0 200px; /* Reduced margin */
+
+  &:first-child {
+    flex: 2;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+  }
+
+  @media (max-width: 903px) {
+    flex: none;
+    margin-bottom: ${spacing.medium};
+  }
+`;
+
+export const FooterTitle = styled.h4`
+  font-size: ${typography.fontSize};
+  font-weight: ${typography.fontWeight.bold};
+  margin-bottom: ${spacing.small};
+  color: #F2F7F4;
 `;
 
 export const FooterLink = styled.a`
-  color: white;
+  color: #F2F7F4;
   text-decoration: none;
-  margin-bottom: ${spacing.small};
+  margin-bottom: ${spacing.large};
+  
   display: block;
-
   &:hover {
     text-decoration: underline;
   }
 `;
 
-export const SubscribeIcons = styled.div`
-  display: flex;
-  gap: ${spacing.small};
-  position: absolute;
-  bottom: ${spacing.small};
-  right: ${spacing.small};
+export const LegalSection = styled.div`
+  flex: 1;
+  margin-left: 38vw;
+  
+  @media (max-width: 903px) {
+    flex: none;
+    margin-bottom: ${spacing.medium};
+  }
 `;
 
-export const SubscribeIcon = styled.a`
-  color: white;
-  font-size: 1.5rem;
+export const NavigationSection = styled.div`
+  flex: 1;
+  margin-left: 5vw; /* Reduced margin */
+
+  @media (max-width: 903px) {
+    flex: none;
+    margin-bottom: ${spacing.medium};
+  }
+`;
+
+export const SubscribeSection = styled.div`
+  flex: 1;
+  margin-left: 5% ; /* Reduced margin */
+
+  @media (max-width: 903px) {
+    flex: none;
+    margin-bottom: ${spacing.medium};
+  }
+`;
+
+export const StoreLinks = styled.div`
+  display: flex;
+  gap: 1vw;
+  margin-top: ${spacing.small};
+
+  @media (max-width: 903px) {
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+    top: 75vh;
+    left: 25vw;
+    transform: translateX(-50%);
+  }
+`;
+
+export const StoreButton = styled.a`
+  display: flex;
+  align-items: center;
+  padding: 4px;
+  border: 2px solid #010A03;
+  border-radius: 10px;
+  text-decoration: none;
+  color: #010a03;
+  background-color: #f2f7f4;
+
+  img {
+    height: 20px;
+    margin-right: 10px;
+  }
+
+  div {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    font-size: 10px;
+    width: 81px;
+
+    div {
+      &:first-child {
+        font-weight: bold;
+      }
+    }
+  }
+`;
+
+export const SubscribeLinks = styled.div`
+  display: flex;
+  gap: ${spacing.small};
+  margin-top: 20px;
 `;
 
 export const SubscribeLink = styled.a`
-  color: white;
+  color: #F2F7F4;
   font-size: 1.5rem;
+`;
+
+export const Form = styled.form`
+  display: flex;
+  align-items: center;
+  border: 3px solid #CEE0D0;
+  border-radius: 16px;
+  overflow: hidden;
+  margin-bottom: ${spacing.large};
+  background-color: #F2F7F4;
+`;
+
+export const Input = styled.input`
+  padding: ${spacing.small};
+  border: none;
+  outline: none;
+  font-size: ${typography.fontSize};
+  width: 300px;
+  border-radius: 30px 0 0 30px;
+  margin: ${spacing.small};
+  background-color: #F2F7F4;
+  color: #010A03;
+`;
+
+export const SubmitButton = styled.button`
+  padding: ${spacing.small} ${spacing.large};
+  background-color: ${colors.primary};
+  color: #F2F7F4;
+  border: none;
+  font-weight: ${typography.fontWeight.bold};
+  cursor: pointer;
+  border-radius: 13px;
+  margin-right: ${spacing.small};
 `;
