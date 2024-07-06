4f4146a63a678739fc3bd81ca9b86894e0d602f9
Dominique Hosea <dominique11h@yahoo.com>
2024-07-05 20:00:03 -0500

    Trending Card height

--- src/components/TrendingCard.tsx
+++ src/components/TrendingCard.tsx
@@ -8,7 +8,7 @@ interface TrendingCardProps {
 
 const TrendingCard: React.FC<TrendingCardProps> = ({ title, image }) => {
   return (
-    <div className="max-w-[246px] h-[300px] w-[272px] p-4 rounded-3xl bg-green-600 text-white text-left mb-10 md:mb-0 flex-shrink-0">
+    <div className="w-[246px] h-[325px] w-[272px] p-4 rounded-3xl bg-green-600 text-white text-left mb-10 md:mb-0 flex-shrink-0">
       <h2 className="w-[240px] h-[84px] font-poppins text-xl font-bold leading-7 text-white mb-2.5">
         {title}
       </h2>
