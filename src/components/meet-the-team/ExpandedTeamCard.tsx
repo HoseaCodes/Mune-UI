3fecaebc1b9d297f66aa4b9a931ce3c38cfab5c9
Riviere Fougy <rivierefougy@gmail.com>
2024-10-03 21:33:21 -0400

    fix: spacing for ExpandedTeamCard text

--- src/components/meet-the-team/ExpandedTeamCard.tsx
+++ src/components/meet-the-team/ExpandedTeamCard.tsx
@@ -31,13 +31,15 @@ export const ExpandedTeamCard: React.FC<{
         className="w-52 h-52 tablet:w-40 tablet:h-40 rounded-full shadow-md border-2 border-custom-bgreen"
       />
       <div className="flex flex-col w-full gap-2">
-        <h3 className="text-black text-base tablet:text-[1.75rem] font-bold">
-          {teamMembers[index].name},{' '}
-          {teamMembers[index].role}
-        </h3>
-        <p className="text-gray-800 text-base tablet:text-[1.375rem] italic">
-          {teamMembers[index].school}
-        </p>
+        <div className="flex flex-col w-full tablet:gap-2">
+          <h3 className="text-black text-base tablet:text-[1.75rem] font-bold">
+            {teamMembers[index].name},{' '}
+            {teamMembers[index].role}
+          </h3>
+          <p className="text-gray-800 text-base tablet:text-[1.375rem] italic">
+            {teamMembers[index].school}
+          </p>
+        </div>
         <div className="flex flex-start w-full text-gray-800 max-w-[63ch]">
           <p className="text-sm tablet:text-base">
             {truncateAfterFirstSentence(
