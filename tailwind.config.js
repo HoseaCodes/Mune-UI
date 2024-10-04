116bc180ea8968308d394961664459d9bb750aa0
Riviere Fougy <rivierefougy@gmail.com>
2024-10-04 15:10:41 -0400

    chore(config): add screen sizes to tailwind config, prettier formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,33 +1,34 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
     extend: {
-      boxShadow: {
-        'glow': '0 0 10px rgba(26, 174, 51, 0.8)', // Customize this as needed
+      screens: {
+        mobile: '320px',
+        tablet: '600px',
+        laptop: '900px',
+        desktop: '1280px',
       },
-      borderRadius:{
+      boxShadow: {
+        glow: '0 0 10px rgba(26, 174, 51, 0.8)', // Customize this as needed
+      },
+      borderRadius: {
         'tl-24': '24px 24px 24px 24px',
       },
-      spacing:{
+      spacing: {
         '48px': '48px',
         '510px': '510px',
         '437px': '437px',
-        '100': '25rem,' 
+        100: '25rem,',
       },
-      colors:{
+      colors: {
         'custom-bgreen': '#cee0d0',
         'custom-green': '#1aae33',
         'custom-purple': '#8133f1',
         'custom-blue': '#33adf1',
         'custom-white': '#f2f7f4',
-
-      }
+      },
     },
   },
-  plugins: [
-    require('tailwind-scrollbar-hide')
-  ],
-}
+  plugins: [require('tailwind-scrollbar-hide')],
+};
